d248813f436a0b85e671e6a0de3b760038a1d2d9
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2016-05-05 07:54:41 +1000

    Build HTML script

--- _scripts/build_html_from_data.rb
+++ _scripts/build_html_from_data.rb
@@ -1,6 +1,33 @@
 #! /usr/bin/env ruby
 #
-# Builds list of products for different languages form the data files.
+# Builds HTML files containing the product lists for different languages form the data files.
+#
+# How to run:
+#
+#   ./_scripts/build_html_from_data.rb
 #
 
-puts("hello")
+def process_single_file(file)
+  puts file
+end
+
+def process_files(files)
+  files.each do |file|
+    process_single_file file
+  end
+end
+
+def run
+  project_root = File.expand_path('../..',__FILE__)
+  data_path = File.join(project_root, '_data')
+
+  unless File.directory?(data_path)
+    puts("ERROR: Directory #{data_path} does not exist")
+  end
+
+  data_files = Dir["#{data_path}/*.tsv"]
+
+  process_files data_files
+end
+
+run
