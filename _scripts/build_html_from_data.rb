124f3185f2b2ea868d7ccc18f13dd8dfc5dc44b2
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2016-05-05 08:09:54 +1000

    Build HTML script

--- _scripts/build_html_from_data.rb
+++ _scripts/build_html_from_data.rb
@@ -11,11 +11,34 @@ def project_root
   File.expand_path('../..',__FILE__)
 end
 
-def process_single_line(line)
-  puts line.split("\t")
+def language_code(file)
+  file_part = File.basename(file).split('data_').reject { |c| c.empty? }.first
+  file_part.split('.tsv').first
+end
+
+def html_file(language_code)
+  destination_dir_path = File.join(project_root, '_includes', 'lists')
+
+  unless File.directory?(destination_dir_path)
+    puts("ERROR: Directory #{destination_dir_path} does not exist")
+  end
+
+  File.join(destination_dir_path, "#{language_code}.html")
 end
 
 def process_single_file(file)
+  language = language_code(file)
+
+  if language.length != 2
+    puts("ERROR: Incorrect data file name #{file}. Should be in format 'data_en.tsv'")
+  end
+
+  destination_file_path = html_file(language)
+
+  # file_parts = file_parts.map { |c|  }
+
+  # .split('.tsv')
+
   File.readlines(file).each do |line|
     process_single_line line
   end
