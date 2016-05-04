1f6b05d154e4e55fab130c62ee379c308f664e4a
Evgenii Neumerzhitckii <sausageskin@gmail.com>
2016-05-05 06:20:35 +1000

    Search JavaScript

--- _includes/js/search.js
+++ _includes/js/search.js
@@ -1,7 +1,35 @@
 (function(){
 
-  document.write('Hello from JavaScript!');
+  function didChangeInput(value) {
+    value = value.toLowerCase();
+    var items = document.querySelectorAll(".List-item");
 
-  // This is a comment. Here we will write JavaScript that draws the simulation.
+    for(var i=0; i < items.length; i++) {
+      var item = items[i];
+      var itemName = item.querySelector(".List-itemName");
+      var itemText = itemName.innerHTML.toLowerCase();
+
+      var dataAttribute = item.getAttribute("data-synonyms");
+
+      if (itemText.indexOf(value) > -1 ||
+        (dataAttribute && dataAttribute.toLowerCase().indexOf(value) > -1)) {
+
+        item.className = "List-item";
+      } else {
+        item.className = "List-item isHidden";
+      }
+    }
+  }
+
+  function init() {
+    var searchInput = document.querySelector(".Search-input");
+
+    // User updates mass in simulation
+    searchInput.addEventListener('input', function() {
+      didChangeInput(searchInput.value);
+    });
+  }
+
+  init();
 
 })();
